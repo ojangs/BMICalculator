71ed232d2977c8236b172ba6424fe265627dfe9b
ojangs <muhirgaming@gmail.com>
2023-08-14 22:51:26 +0700

    Fitur Utama dan Splashscreen DONE !!!

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.bmicalculator.databinding.ActivityMainBinding
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -24,15 +26,49 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+        // Menambahkan listener pada EditText
+        binding.isiBerat.addTextChangedListener(textWatcher)
+        binding.isiTinggi.addTextChangedListener(textWatcher)
 
+        // Menjalankan validasi awal
+        validateButton()
 
             binding.btnHitung.setOnClickListener() {
-                myCoroutineScope.launch {
-                    getBmiIndex()
-                    weightCategoryBMI()
+                if(binding.btnHitung.isEnabled){
+                    myCoroutineScope.launch {
+                        getBmiIndex()
+                        weightCategoryBMI()
+                    }
                 }
+
             }
 
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            validateButton()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // Do nothing
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            // Do nothing
+        }
+    }
+
+    private fun validateButton() {
+        val inputText1 = binding.isiBerat.text.toString().trim()
+        val inputText2 = binding.isiTinggi.text.toString().trim()
+
+        // Validasi apakah salah satu dari inputText kosong atau berisi "0"
+        val isInputInvalid = inputText1.isEmpty() || inputText1 == "0" ||
+                inputText2.isEmpty() || inputText2 == "0"
+
+        // Mengaktifkan atau menonaktifkan tombol berdasarkan validasi
+        binding.btnHitung.isEnabled = !isInputInvalid
     }
 
     private fun getBmiIndex(): Double{
